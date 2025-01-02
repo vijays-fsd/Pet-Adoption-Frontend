cc28aa32cd4a147123c43c24b5ec51333f4c1ef8
vijays-fsd <vijay2003sankar@gmail.com>
2025-01-03 00:26:15 +0530

    url updated

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api/v1/",
+    // baseURL: "http://localhost:5000/api/v1/",
+    baseURL: "https://pet-adoption-backend-9znb.onrender.com",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json"
